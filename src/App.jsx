6e3db77930cc213e3f26844a3d385388f48f7158
Olexandr_Kryvoruchko <frostmet@gmail.com>
2025-05-09 05:38:33 +0300

    add start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
